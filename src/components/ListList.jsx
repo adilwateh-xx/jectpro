024af0eb523f13302cd423b29eda5dfbaf25ee23
adilwateh-xx <6210110429@psu.ac.th>
2024-07-14 21:38:08 +0700

    stikylist

---
+++ src/components/ListList.jsx
@@ -0,0 +1,21 @@
+function ListItem({ list }) {
+  return (
+   <div>
+    {
+        list.map((l) => (
+            <dir className="flex cursor-pointer justify-between my-3">
+                <div className="ml-8 flex">
+                    
+                    <div className="flex items-center  ">{l.icon}</div>
+                    <h1 className="ml-3">{l.message}</h1>
+                </div> 
+                
+                {l.count}
+            </dir>
+        ))
+    }
+   </div>
+  )
+}
+
+export default ListItem
