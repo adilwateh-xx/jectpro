aba44954bdef2324a4cd2aa5bf8ceb7e7e1658c2
HimRahim <him.wanrahim@gmail.com>
2024-07-17 22:53:32 +0700

    add state

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ import Tags from "./Tags";
 function Sidebar() {
   return (
     <div className="sidebar flex border w-[30%] h-full">
-      <div className="sidebar-container shadow-md rounded-xl bg-[#f5f5f5] flex flex-col w-full m-4 border py-3 px-4">
+      <div className="sidebar-container shadow-md rounded-xl bg-[#f5f5f5] flex flex-col w-full m-4 border py-3 px-4 overflow-auto">
         <div className="flex justify-between">
           <h1 className="text-2xl font-semibold">Menu</h1>
           <div className="flex items-center mr-3 cursor-pointer">
--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,18 +1,31 @@
+import { useState } from "react"
 
 
 function Tags() {
-    const list = [
+    const tagList = [
         'tag1',
         'tag2',
-        'tag3'
+        'tag3',
+        'tag3',
+        'tag3',
+        'tag3',
+        'tag3',
+        '',
     ]
+
+    const [list, setList] = useState(tagList)
+
+    function addTag(tag) {
+        list.pop()
+        setList([...list, tag, ''])
+    }
 
    
     return (
         <div className="w-full">
             <div className='flex'>TAGS</div>
                 <div>
-                    <ListTags list={list} />
+                    <ListTags list={list} onClick={() => addTag('new')} />
                 </div> 
         </div>
       
@@ -21,14 +34,14 @@ function Tags() {
 
 export default Tags
 
- function ListTags({ list }){
+ function ListTags({ list, onClick }){
   return (
-   <div className="flex px-2">
+   <div className="grid sm:grid-cols-2 md:grid-cols-3  px-2">
     {
         list.map((l) => (
-            <div key={l} className="border flex cursor-pointer my-3 mx-1 rounded-xl justify-center">
+            <div onClick={l ? () => {}: onClick} key={l} className="border flex cursor-pointer my-3 mx-1 rounded-xl justify-center">
                 <div className="px-5 py-2">
-                    <h1>{l}</h1>
+                    <h1>{l ? l : '+'}</h1>
                 </div>
             </div>
         ))
